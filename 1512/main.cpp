b824519adc7503c4bfa28fb8fca782bfe4a54925
starHo4 <hi6yukiho4@gmail.com>
2021-08-10 20:34:41 +0900

    Not yet ..

---
+++ 1512/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+typedef long long ll;
+int main(){
+    int N;
+    cin >> N;
+    vector<string> S(N);
+    for(int i=0; i<N; i++){
+        cin >> S[i];
+    }
+    vector<ll> dp(N);
+}
